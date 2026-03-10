ccfd65c7553b13dbd5df443d88f4931a467af9bf
SushritaBiswas <sushritabiswas19@gmail.com>
2026-03-10 10:50:37 +0530

    Ui Fix for the Broken Image

--- client/src/data/Feedback.data.ts
+++ client/src/data/Feedback.data.ts
@@ -3,7 +3,7 @@ export const FeedbackData = [
     name: "Ralph Edwards",
     company: "Big Kahuna Burger Ltd",
     designation: "Senior Software Developer",
-    image: "/HomeImg/human4.webp",
+    image: "/HomeImg/human1.webp",
     testimonial:
       "I found TailAdmin perfect. The code is easy to use in my projects, some of which are already in production.",
   },
@@ -27,7 +27,7 @@ export const FeedbackData = [
     name: "Esther Howard",
     company: "Barone LLC.",
     designation: "Senior Software Developer",
-    image: "/HomeImg/human3.webp",
+    image: "/HomeImg/human1.webp",
     testimonial:
       "I found TailAdmin perfect. The code is easy to use in my projects, some of which are already in production.",
   },
@@ -35,7 +35,7 @@ export const FeedbackData = [
     name: "Darlene Robertson",
     company: "Abstergo Ltd.",
     designation: "Senior Software Developer",
-    image: "/HomeImg/human4.webp",
+    image: "/HomeImg/human2.webp",
     testimonial:
       "I found TailAdmin perfect. The code is easy to use in my projects, some of which are already in production.",
   },
