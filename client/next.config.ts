f5902c7d6212042d67f2294098b8adfc3c611a0d
SushritaBiswas <sushritabiswas19@gmail.com>
2026-09-17 00:27:26 +0530

    New SRC Updated and Tested

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,16 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
